e37aac8b4acdf1b5348c6f705f29be3526023547
oyps <ponconsoft@gmail.com>
2023-01-05 13:27:54 +0800

    ..

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import ReactDOM from "react-dom/client";
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Box component="span" sx={{ p: 2, border: '1px dashed grey' }}>
-    <Button>Save</Button>
-</Box>);5
